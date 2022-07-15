f8ee616a0b4d1d5c5c62511ae24e77c5e5382963
«Denis» <denis.ch.9797@mail.ru>
2022-07-15 22:05:32 +0500

    homeworks7

---
+++ homework2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в 
+//двумерном массиве, и возвращает значение этого элемента или же указание, 
+//что такого элемента нет
+
+int value;
+Console.WriteLine("Введите число");
+int[,] arr = new int[3, 4]
+{
+    {1, 4, 7, 2},
+    {5, 9, 2, 3}, 
+    {8, 4, 2, 4}, 
+};
+int.TryParse(Console.ReadLine(), out value);
+for (int i = 0; i < arr.Length / 4; i++)
+{
+    for (int j = 0; j < arr.Length / 3; j++)
+    {
+        if (arr[i, j] == value) Console.WriteLine("Индекс равен = [{0},{1}]", i, j);
+    }
+    
+}
---
+++ homework3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое 
+//элементов в каждом столбце.
+
+int[,] array = new int[3, 4];
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        array[i, j] = i * j + 1;
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < 4; i++)
+{
+    int summ = 0;
+    for (int j = 0; j < 3; j++)
+    {
+        summ = summ + array[j, i];
+    }
+    double average = summ / 3;
+    int n = i + 1;
+    Console.WriteLine("Среднее значение " + n + " столбец " + average);
+}
